8c2c5163738c95de795dc413eeb67c2014fa50cd
jeevassomasundaram <jeeva.somasundaram@in.ey.com>
2022-04-20 11:55:32 +0530

    Home page changes

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -17,13 +17,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'dealerhome'.tr,
-          style: TextStyle(
-            fontSize: 20,
-            letterSpacing: 0.15,
-          ),
-        ),
+        title: Text('dealerhome'.tr),
         backgroundColor: Color.fromRGBO(227, 30, 48, 1),
         actions: [
           IconButton(
@@ -47,29 +41,174 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
           child: Column(children: <Widget>[
-        GestureDetector(
-          child: Text('applyfordealership'.tr,
-              style: TextStyle(
-                  decoration: TextDecoration.underline, color: Colors.orange)),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => OnBoardOnePage()),
-            );
-          },
+        Container(
+            padding: EdgeInsets.all(15),
+            margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+            decoration: new BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: [
+                  new BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 5.0,
+                  ),
+                ]),
+            child: Column(
+              children: [
+                Row(
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color(0xffE31E30),
+                        ),
+                        child: Text(
+                          'D',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        )),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(30, 20, 20, 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Welcome Dealer Name'.tr,
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                                fontSize: 18,
+                              )),
+                          Text('IDNumber'.tr,
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.black,
+                                fontSize: 18,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Card(
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  color: Colors.red,
+                  child: Image.asset(
+                    'images/shree-cement-500x500_2.png',
+                    fit: BoxFit.fill,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  elevation: 5,
+                  margin: EdgeInsets.all(7),
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color(0xffE31E30),
+                        onPrimary: const Color(0xffffffff),
+                        textStyle: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xffffffff),
+                        )),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => HomePage()));
+                    },
+                    child: Text('Place New Order'.toUpperCase()))
+              ],
+            )
+            // child: Text('Welcome'.tr,
+            //     style: TextStyle(
+            //         fontWeight: FontWeight.bold, color: Colors.black,
+            //     fontSize: 18,
+            //     )),
+            ),
+        Container(
+          width: double.infinity,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          padding: EdgeInsets.all(15),
+          margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+          decoration: new BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: [
+                new BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 5.0,
+                ),
+              ]),
+          child: Column(
+            children: <Widget>[
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: const Color(0xffE31E30),
+                      onPrimary: const Color(0xffffffff),
+                      textStyle: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xffffffff),
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  },
+                  child: Text('Past Orders'.toUpperCase())),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: const Color(0xffE31E30),
+                      onPrimary: const Color(0xffffffff),
+                      textStyle: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xffffffff),
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  },
+                  child: Text('Orders waiting under evaluation'.toUpperCase()))
+            ],
+          ),
         ),
-        ElevatedButton(
-            onPressed: () {
-              var locale = Locale('en', 'US');
-              Get.updateLocale(locale);
-            },
-            child: Text('english'.tr)),
-        ElevatedButton(
-            onPressed: () {
-              var locale = Locale('hi', 'IN');
-              Get.updateLocale(locale);
-            },
-            child: Text('hindi'.tr))
+        // Card(
+        //   semanticContainer: true,
+        //   clipBehavior: Clip.antiAliasWithSaveLayer,
+        //   color: Colors.red,
+        //   child: Image.network(
+        //     'https://www.shreecement.com/uploads/news-list/shree-cement-500x500_2.png',
+        //     fit: BoxFit.fill,
+        //   ),
+        //   shape: RoundedRectangleBorder(
+        //     borderRadius: BorderRadius.circular(10.0),
+        //   ),
+        //   elevation: 5,
+        //   margin: EdgeInsets.all(10),
+        // ),
+        //     GestureDetector(
+        //       child: Text('applyfordealership'.tr,
+        //           style: TextStyle(
+        //               fontWeight: FontWeight.bold, color: Colors.blueAccent)),
+        //       onTap: () {
+        //         Navigator.push(
+        //           context,
+        //           MaterialPageRoute(builder: (context) => OnBoardOnePage()),
+        //         );
+        //       },
+        //     ),
+
+        // ElevatedButton(
+        //     onPressed: () {
+        //       var locale = Locale('en', 'US');
+        //       Get.updateLocale(locale);
+        //     },
+        //     child: Text('english'.tr)),
+        // ElevatedButton(
+        //     onPressed: () {
+        //       var locale = Locale('hi', 'IN');
+        //       Get.updateLocale(locale);
+        //     },
+        //     child: Text('hindi'.tr))
       ])),
       drawer: const DrawerPage(),
     );
